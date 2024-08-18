14a1e953d46d8c425e16c37cd802f9e4b3295e82
Rashi2006 <140819546+Rashi2006@users.noreply.github.com>
2024-08-18 09:59:32 +0530

    Programs till !8/8/24

--- College Lab/Express/CRUDusingExpress/UsingArray.js
+++ College Lab/Express/CRUDusingExpress/UsingArray.js
@@ -1,6 +1,34 @@
 const express = require('express');
 const app = express();
 
+const students = ['Rashi' , 'Pretty' , 'Smart' , 'Focused'];
+
+//Read
+app.get('/students',(req,res)=>{
+    res.send(students);
+});
+app.get('/students/:id',(req,res)=>{
+    res.send(students[req.params.id]);
+});
+
+//Create
+app.post('/students/:name',(req,res)=>{
+     students.push(req.params.name);
+     res.send('Student added successfully');
+});
+
+//Update
+app.patch('/students/:idx/:name',(req,res)=>{
+    students[req.params.idx] = req.params.name;
+    res.send('Student updated successfully');
+});
+
+//Delete
+app.delete('/students/:idx',(req,res)=>{
+    students.splice(req.params.idx,1);
+    res.send('Student deleted successfully');
+});
+
 app.listen(4567,()=>{
     console.log('listening on port 4567');
 })
--- College Lab/Express/CRUDusingExpress/UsingArrayOfObject.js
+++ College Lab/Express/CRUDusingExpress/UsingArrayOfObject.js
@@ -17,19 +17,41 @@ let students = [
     }
 ];
 
+//Read
 app.get('/students',(req,res)=>{
     res.send(students);
 });
 
 app.get('/students/:index',(req,res)=>{
-     res.send(students[req.params.index]);
+     const ans = students.find(stu=>{
+        return stu.id == req.params.index;
+     });
+     res.send(ans);
 });
 
+//SEARCH
+app.get('/students/serach/:text',(req,res)=>{
+    const ans = students.filter(stu=>{
+        return stu.name.toLowerCase().includes(req.params.text.toLowerCase());
+    });
+    res.send(ans);
+});
+
+//Filter
+app.get('/students/:from/:to' , (req,res)=>{
+    const ans = students.filter(stu=>{
+        return stu.age >= (req.params.from) && stu.age <= (req.params.to);
+    });
+    res.send(ans);
+});
+
+//create
 app.post('/students',(req,res)=>{
      students.push(req.body);
      res.send('data posted');
 });
 
+//update
 app.patch('/students/:id',(req,res)=>{
     // const ans = students.findIndex((stu)=>{
     //     if(stu.id==req.params.index){
@@ -42,11 +64,13 @@ app.patch('/students/:id',(req,res)=>{
     res.send('data updated');
 });
 
+//delete
 app.delete('/students/:id',(req,res)=>{
     const ans = students.findIndex(stu => stu.id == req.params.id);
     students.splice(ans,1);
     res.send('data deleted');
 });
+
 
 app.listen(4004, () => {
     console.log('listening on 4004');
